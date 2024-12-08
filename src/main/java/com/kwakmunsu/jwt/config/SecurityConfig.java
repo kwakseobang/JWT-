30aa146f147fcf50c46f3bc209ce4e70e7f5af33
kwakseobang <iii6602@naver.com>
2024-12-08 18:12:50 +0900

    기본 설정

---
+++ src/main/java/com/kwakmunsu/jwt/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.kwakmunsu.jwt.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // 암호화
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //csrf disable - session을 stateless 상태로 두기때문에 비활성화
+        http
+                .csrf((auth) -> auth.disable());
+
+        //From 로그인 방식 disable - jwt를 사용하기때문에
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //http basic 인증 방식 disable  - jwt를 사용하기때문에
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        // 경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+        // 세션 설정 stateless로 설정해야됨.
+        // JWT를 통한 인증/인가를 위해서 세션을 STATELESS 상태로 설정하는 것이 중요하다.
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/kwakmunsu/jwt/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.kwakmunsu.jwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminP() {
+
+        return "admin Controller";
+    }
+}
