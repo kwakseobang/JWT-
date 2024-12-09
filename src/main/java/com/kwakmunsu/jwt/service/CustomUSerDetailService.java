3e3295d49db62a51ffbf6ead64d6a36243a26ff3
kwakseobang <iii6602@naver.com>
2024-12-09 23:05:28 +0900

    UserDetails update

--- src/main/java/com/kwakmunsu/jwt/service/CustomUSerDetailService.java
+++ src/main/java/com/kwakmunsu/jwt/service/CustomUSerDetailService.java
@@ -1,12 +1,11 @@
 package com.kwakmunsu.jwt.service;
 
 import com.kwakmunsu.jwt.domain.User;
+import com.kwakmunsu.jwt.dto.CustomUserDetails;
 import com.kwakmunsu.jwt.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -23,19 +22,13 @@ public class CustomUSerDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        return userRepository.findByUsername(username)
-                .map(this::createUserDetails)
-                .orElseThrow(() -> new UsernameNotFoundException("not Found"));
-    }
-    private UserDetails createUserDetails(User user) {
-        // Spring Security는 내부적으로 사용자의 권한을 GrantedAuthority 객체로 관리합니다.
-        //user.getRole()이 반환하는 값이 단순 문자열이라면, 이를 Spring Security가 인식하지 못합니다.
-        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getRole().toString());
+        // 사용자 조회
+        User userData = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                Collections.singleton(grantedAuthority) // 단일 권한
-        );
+        // CustomUserDetails로 반환
+        return new CustomUserDetails(userData);
     }
+
+
 }
